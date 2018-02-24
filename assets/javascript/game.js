d88c03d33369a694aed006e043ab271cfc312565
Brita Ramsay <britaramsay@gmail.com>
2018-02-24 14:26:58 -0500

    Users keys saved as a variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,24 @@
-var monsters = ["banshee", "basilisk", "behemoth", 
-            "centaur", "cyclops", "dryad", "elf", 
-            "ghoul", "imp", "ogre", "orc", 
-            "troll", "zombie"];
+var monsters = ["banshee", "basilisk", "behemoth", "centaur", 
+                "cyclops", "dryad", "elf", "ghoul", "imp", 
+                "ogre", "orc", "troll", "zombie"];
 
 function newGame() {
+    //Choose a random monster for user to guess
     var monster = monsters[Math.floor(Math.random()*monsters.length)];
     console.log(monster);
+
+    //Show instructions when new game is started
+    var x = document.getElementById("instructions");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+
+    document.onkeyup = function(event) {
+        var guess = event.key;
+        console.log(guess);
+    };
+
+
 }
